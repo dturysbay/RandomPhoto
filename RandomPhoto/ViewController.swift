d1e74aaf7c69882cdb61104fb66c7eda7311fa7b
Dinmukhambet Turysbay <d.turysbay@yandex.kz>
2022-11-22 18:07:34 +0600

    random button, color randomizer has been added

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -16,16 +16,48 @@ class ViewController: UIViewController {
         return imageView
     }()
     
-
-
+    private let button : UIButton = {
+            let button = UIButton()
+            button.backgroundColor = .white
+            button.setTitle("Random Photo", for: .normal)
+            button.setTitleColor(.black, for: .normal)
+            return button
+    }()
+    
+    let colors: [UIColor] = [
+        .systemPink,
+        .systemBlue,
+        .systemGreen,
+        .systemYellow,
+        .systemPurple,
+        .systemOrange
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
+        view.addSubview(button)
         getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
+    
+    @objc func didTapButton(){
+        getRandomPhoto()
+        view.backgroundColor = colors.randomElement()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        button.frame = CGRect(
+            x: 30,
+            y:view.frame.size.height-150-view.safeAreaInsets.bottom,
+            width: view.frame.size.width-60,
+            height: 55)
+    }
+    
+    
    
     func getRandomPhoto(){
         let urlString = "https://source.unsplash.com/random/600x600"
